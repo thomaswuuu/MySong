c10df2df9db2168d4fdd51a31fbf4d8be3f4f098
numb1086 <numb1086@users.noreply.github.com>
2024-01-27 14:07:50 +0800

    [Update] Add login, singup features and user model

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const JWT = require("jsonwebtoken");
+const JWTSecret = process.env.JWT_SECRET;
 
-router.get("/", async (req, res) => {
-  return res.render("profile", { user: null });
+const JWTAuthCheck = (req, res, next) => {
+  const token = req.cookies.jwt_token;
+  if (!token) {
+    req.flash("error_msg", "登入憑證不存在，請重新登入。");
+    return res.redirect("/auth/login");
+  }
+
+  JWT.verify(token, JWTSecret, (err, decoded) => {
+    if (err) {
+      req.flash("error_msg", "登入憑證已過期，請重新登入。");
+      return res.redirect("/auth/login");
+    }
+    next();
+  });
+};
+
+router.get("/", JWTAuthCheck, async (req, res) => {
+  return res.render("profile", { user: req.user });
 });
 
 module.exports = router;
