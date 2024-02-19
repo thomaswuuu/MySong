d7353bf2c47f4422b1745fa2373ec742556d661d
numb1086 <numb1086@users.noreply.github.com>
2024-02-19 15:01:59 +0800

    Initial swagger feature

--- routes/api.js
+++ routes/api.js
@@ -7,34 +7,270 @@ const auth = require("../controllers/authController");
 const passport = require("passport");
 
 // Login and create token
-router.post("/login", passport.authenticate("local"), auth.createAuthToken);
+router.post(
+  "/login",
+  /* #swagger.tags = ["login"]
+     #swagger.summary = 'Login MySong App'
+     #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+                $ref: "#/components/schemas/login"
+            }
+          }
+        }
+      }
+   */
+  passport.authenticate("local"),
+  auth.createAuthToken
+);
 
 // Add middleware and check login token for super user
 router.use("/charts", auth.superAuthCheck);
-router.get("/charts", indexController.getCharts);
-router.post("/charts", indexController.setCharts);
-router.put("/charts", indexController.updateCharts);
-router.delete("/charts", indexController.deleteCharts);
+router.get(
+  "/charts",
+  /* #swagger.tags = ["charts"]
+     #swagger.summary = 'Get chart list of queried platform'
+     #swagger.parameters['platform'] = {
+        in: 'query',
+        required: 'true',
+        description: 'Platform(KKBOX/Spotify)',
+        type: 'string'
+     }
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  indexController.getCharts
+);
+
+router.post(
+  "/charts",
+  // #swagger.ignore = true
+  indexController.setCharts
+);
+router.put(
+  "/charts",
+  /* #swagger.tags = ["charts"]
+     #swagger.summary = 'Update chart list of queried platform'
+     #swagger.parameters['platform'] = {
+        in: 'query',
+        required: 'true',
+        description: 'Platform(KKBOX/Spotify)',
+        type: 'string'
+     }
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  indexController.updateCharts
+);
+router.delete(
+  "/charts",
+  // #swagger.ignore = true
+  indexController.deleteCharts
+);
 
 router.use("/tracks", auth.superAuthCheck);
-router.get("/tracks", indexController.getTracks);
-router.post("/tracks", indexController.setTracks);
-router.put("/tracks", indexController.updateTracks);
-router.delete("/tracks", indexController.deleteTracks);
+router.get(
+  "/tracks",
+  /* #swagger.tags = ["tracks"]
+     #swagger.summary = 'Get track list of queried platform and playlist_id'
+     #swagger.parameters['platform'] = {
+        in: 'query',
+        required: 'true',
+        description: 'Platform(KKBOX/Spotify)',
+        type: 'string'
+     }
+     #swagger.parameters['id'] = {
+        in: 'query',
+        required: 'true',
+        description: 'Playlist Id',
+        type: 'string'
+     }
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  indexController.getTracks
+);
+router.post(
+  "/tracks",
+  // #swagger.ignore = true
+  indexController.setTracks
+);
+router.put(
+  "/tracks",
+  /* #swagger.tags = ["tracks"]
+     #swagger.summary = 'Update track list of queried platform and playlist_id'
+     #swagger.parameters['platform'] = {
+        in: 'query',
+        required: 'true',
+        description: 'Platform(KKBOX/Spotify)',
+        type: 'string'
+     }
+     #swagger.parameters['id'] = {
+        in: 'query',
+        required: 'true',
+        description: 'Playlist Id',
+        type: 'string'
+     }
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  indexController.updateTracks
+);
+router.delete(
+  "/tracks",
+  // #swagger.ignore = true
+  indexController.deleteTracks
+);
 
 router.use("/users", auth.superAuthCheck);
-router.get("/users", userController.getAllUsers);
-router.get("/users/:user_id", userController.getUser);
-router.post("/users", userController.setUser);
-router.put("/users/:user_id", userController.updateUser);
-router.delete("/users/:user_id", userController.deleteUser);
+router.get(
+  "/users",
+  /* #swagger.tags = ["users"]
+     #swagger.summary = 'Get all users list'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  userController.getAllUsers
+);
+router.get(
+  "/users/:user_id",
+  /* #swagger.tags = ["users"]
+     #swagger.summary = 'Get a specific user with user_id'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  userController.getUser
+);
+router.post(
+  "/users",
+  /* #swagger.tags = ["users"]
+     #swagger.summary = 'Add a new user'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+     #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+                $ref: "#/components/schemas/users"
+            }
+          }
+        }
+      }
+   */
+  userController.setUser
+);
+router.put(
+  "/users/:user_id",
+  /* #swagger.tags = ["users"]
+     #swagger.summary = 'Update user info with user_id'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+     #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+                $ref: "#/components/schemas/users"
+            }
+          }
+        }
+      }
+   */
+  userController.updateUser
+);
+router.delete(
+  "/users/:user_id",
+  /* #swagger.tags = ["users"]
+     #swagger.summary = 'Delete a user with user_id'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  userController.deleteUser
+);
 
 // Add middleware and check login token for normal user
 router.use("/follows", auth.normalAuthCheck);
-router.get("/follows", profileController.getUserAllFollows);
-router.get("/follows/:track_id", profileController.getUserFollow);
-router.post("/follows", profileController.setUserFollow);
-router.put("/follows/:track_id", profileController.updateUserFollow);
-router.delete("/follows/:track_id", profileController.deleteUserFollow);
+router.get(
+  "/follows",
+  /* #swagger.tags = ["follows"]
+     #swagger.summary = 'Get all following tracks of current login user'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  profileController.getUserAllFollows
+);
+router.get(
+  "/follows/:track_id",
+  /* #swagger.tags = ["follows"]
+     #swagger.summary = 'Get a specific following track with track_id'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  profileController.getUserFollow
+);
+router.post(
+  "/follows",
+  /* #swagger.tags = ["follows"]
+     #swagger.summary = 'Add a new following track'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+     #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+                $ref: "#/components/schemas/follows"
+            }
+          }
+        }
+      }
+   */
+  profileController.setUserFollow
+);
+router.put(
+  "/follows/:track_id",
+  /* #swagger.tags = ["follows"]
+     #swagger.summary = 'Update a specific following track with track_id'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+     #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+                $ref: "#/components/schemas/follows"
+            }
+          }
+        }
+      }
+   */
+  profileController.updateUserFollow
+);
+router.delete(
+  "/follows/:track_id",
+  /* #swagger.tags = ["follows"]
+     #swagger.summary = 'Delete a specific following track with track_id'
+     #swagger.security = [{
+        "bearerAuth": []
+      }]
+   */
+  profileController.deleteUserFollow
+);
 
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,49 @@
+const swaggerAutogen = require("swagger-autogen")({ openapi: "3.0.0" });
+
+const doc = {
+  info: {
+    version: "1.0.0", // by default: '1.0.0'
+    title: "MySong", // by default: 'REST API'
+    description: "Document of OpenAPI for MySong App",
+  },
+  servers: [
+    {
+      url: "http://localhost:8080/api",
+      description: "",
+    },
+  ],
+  components: {
+    schemas: {
+      login: {
+        $username: "admin@gmail.com",
+        $password: "root123456",
+      },
+      users: {
+        $name: "tester",
+        $email: "tester@example.com",
+        $password: "password",
+        $thumbnail: "",
+      },
+      follows: {
+        $type: "KKBOX",
+        $track_id: "Gt0DxJP--2a5-4a5C4",
+        $title: "Shape of You",
+        $titleLink: "https://www.kkbox.com/tw/tc/song/Gt0DxJP--2a5-4a5C4",
+        $artist: "Ed Sheeran (紅髮艾德)",
+        $artistLink: "https://www.kkbox.com/tw/tc/artist/LZA5o1V1w4lbj0Rq2k",
+        $cover: "https://i.kfs.io/album/global/21672225,1v1/fit/160x160.jpg",
+      },
+    },
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger-out.json";
+const routes = ["./routes/api.js"];
+
+swaggerAutogen(outputFile, routes, doc);
